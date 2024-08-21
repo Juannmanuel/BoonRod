bf878dd7289dab951e113dc3607850ad1a596bf5
Juan Manuel <losredondos551@gmail.com>
2024-08-20 21:24:42 -0300

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Route, Routes } from "react-router";
 import Navbar from "../src/components/Navbar/Navbar";
 import Covers from "./components/Covers/Covers";
 import Detail from "./pages/Detail/Detail";
@@ -7,7 +8,6 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 
 function App() {
-
   // Estado para manejar el slide actual
   const [currentSlide, setCurrentSlide] = useState(0);
 
@@ -20,16 +20,20 @@ function App() {
 
   return (
     <div className="App">
-      {/* Pasamos las secciones y el slide actual al componente Navbar */}
       <Navbar sections={sections} currentSlide={currentSlide} />
-      {/* Pasamos las categorías, la función de cambio de slide y el slide actual al componente Covers */}
-      {/* <Covers
-        sections={sections}
-        handleSlideChange={handleSlideChange}
-        currentSlide={currentSlide}
-      /> */}
-      {/* <Detail sections={sections} currentSlide={currentSlide}/> */}
-      <Home sections={sections} handleSlideChange={handleSlideChange} currentSlide={currentSlide}/>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              sections={sections}
+              handleSlideChange={handleSlideChange}
+              currentSlide={currentSlide}
+            />
+          }
+        />
+        <Route path="/detail" element={<Detail  sections={sections}  currentSlide={currentSlide}/>}/>
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import logoImage from "../../assets/images/logo/logo_boonrod_oscuro.png";
+import { useLocation } from 'react-router-dom';
 import { FaAngleDoubleDown, FaAngleDoubleUp } from "react-icons/fa";
 
 const Navbar = ({ sections, currentSlide }) => {
+
+  const location = useLocation();
+  const currentPath = location.pathname;
 
   return (
     <nav className={styles.navbar}>
       <img src={logoImage} alt="Logo de la tienda" className={styles.logo} />
       <div className={styles.link}>
-        {sections.map((item, index) => (
+        { currentPath !== "/detail" ? sections.map((item, index) => (
           <button
             className={`${styles.cta} ${
               currentSlide === index ? styles.active : ""
@@ -19,7 +23,7 @@ const Navbar = ({ sections, currentSlide }) => {
           >
             <span className={styles.hover_underline_animation}>{item.section}</span>
           </button>
-        ))}
+        )) : null}
       </div>
     </nav>
   );
