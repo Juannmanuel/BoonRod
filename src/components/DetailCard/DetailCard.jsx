de5d00b2604343f1f2d985317ac9aa46e3c94198
Juan Manuel <losredondos551@gmail.com>
2024-08-23 01:42:23 -0300

    detail

--- src/components/DetailCard/DetailCard.jsx
+++ src/components/DetailCard/DetailCard.jsx
@@ -4,27 +4,32 @@ import style from "./DetailCard.module.css";
 function DetailCard() {
   return (
     <section className={style.detailCard_main}>
+      <div className={style.detail_product}>
       <div className={style.productHeader}>
         <h2 className={style.productName}>PANTALÓN CHINO PLIEGUES</h2>
-        <br />
         <span className={style.productPrice}>$15.000</span>
-      </div>
-      <p className={style.productDescription}>
+        <p className={style.productDescription}>
         Pantalón regular fit confeccionado en tejido de algodón con elasticidad.
         Cintura con detalle de pliegues frontales. Bolsillos en delantero y
         detalle de bolsillos traseros de vivo. Cierre frontal con cremallera y
         botón.
       </p>
+      </div>
       <div className={style.productSizes}>
         <span>Talles disponibles: S, M, L, XL</span>
       </div>
-      <div className={style.productActions}>
+      <div className={style.productActions_btn}>
         <button>Añadir al carrito</button>
         <button>Comprar ahora</button>
       </div>
+      </div>
+      <div className={style.recommendedProductos_container}></div>
+      {/* 
+     
+     
       <div className={style.recommendedProductos_container}>
         <RecommendedProducts />
-      </div>
+      </div> */}
     </section>
   );
 }
