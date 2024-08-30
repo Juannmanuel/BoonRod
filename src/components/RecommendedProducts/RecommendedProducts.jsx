ebae3be084a50bff98efd1c5cd3fbc3c70ee7ebe
Juan Manuel <losredondos551@gmail.com>
2024-08-29 21:50:35 -0300

    reccommended

--- src/components/RecommendedProducts/RecommendedProducts.jsx
+++ src/components/RecommendedProducts/RecommendedProducts.jsx
@@ -1,25 +1,53 @@
-import style from "./RecommendedProducts.module.css"
+import style from "./RecommendedProducts.module.css";
+import imagenModelo1 from "../../assets/images/Ropa/Producto_1_Camisa/Modelo/Modelo_1.jpg";
+import imagenModelo2 from "../../assets/images/Ropa/Producto_1_Camisa/Modelo/Modelo_2.jpg";
+import imagenModelo3 from "../../assets/images/Ropa/Producto_1_Camisa/Modelo/Modelo_3.jpg";
+import Sliders from "../Sliders/Sliders";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-
-function RecommendedProducts(){
-
-    return (
-        <section className={style.RecommendedProducts_main}>
-
-            <div className={style.item}></div>
-            <div className={style.item}></div>
-            <div className={style.item}></div>
-            <div className={style.item}></div>
-            <div className={style.item}></div>
-            <div className={style.item}></div>
-            <div className={style.item}></div>
-            <div className={style.item}></div>
-            <div className={style.item}></div>
-            <div className={style.item}></div>
-            <div className={style.item}></div>
-            <div className={style.item}></div>
-        </section>
-    )
+function RecommendedProducts() {
+  const images = [imagenModelo1, imagenModelo2, imagenModelo3];
+  return (
+    <section className={style.RecommendedProducts_main}>
+      <div className={style.card}>
+        <div  className={style.image_card}></div>
+        <div className={style.description_card}></div>
+      </div>
+      <div className={style.card}>
+        <div  className={style.image_card}></div>
+        <div className={style.description_card}></div>
+      </div>
+      <div className={style.card}>
+        <div  className={style.image_card}></div>
+        <div className={style.description_card}></div>
+      </div>
+      <div className={style.card}>
+        <div  className={style.image_card}></div>
+        <div className={style.description_card}></div>
+      </div>
+      <div className={style.card}>
+        <div  className={style.image_card}></div>
+        <div className={style.description_card}></div>
+      </div>
+      <div className={style.card}>
+        <div  className={style.image_card}></div>
+        <div className={style.description_card}></div>
+      </div>
+      <div className={style.card}>
+        <div  className={style.image_card}></div>
+        <div className={style.description_card}></div>
+      </div>
+      <div className={style.card}>
+        <div  className={style.image_card}></div>
+        <div className={style.description_card}></div>
+      </div>
+      <div className={style.card}>
+        <div  className={style.image_card}></div>
+        <div className={style.description_card}></div>
+      </div>
+    </section>
+  );
 }
 
-export default RecommendedProducts
+export default RecommendedProducts;
--- src/pages/CatalogGrid/CatalogGrid.jsx
+++ src/pages/CatalogGrid/CatalogGrid.jsx
@@ -1,10 +1,11 @@
+import RecommendedProducts from "../../components/RecommendedProducts/RecommendedProducts";
 import style from "./CatalogGrid.module.css";
 
 function CatalogGrid() {
   return (
     <section className={style.CatalogGrid_main}>
-    <div className={style.section_up}></div>
-    <div className={style.section_down}></div>
+      <div className={style.primeraImagen}></div>
+      <RecommendedProducts/>
     </section>
   );
 }
