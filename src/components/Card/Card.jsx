855176a468cbcaa6302007418b138f89d8be2a8c
Juan Manuel <losredondos551@gmail.com>
2024-09-02 00:22:14 -0300

    catalogo

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,15 @@
-import style from "./Card.module.css"
-import imagen from "../../assets/images/Ropa/Producto_5_Remera/Modelo/Modelo_2.jpg"
+import style from "./Card.module.css";
+import imagen from "../../assets/images/Ropa/Producto_5_Remera/Modelo/Modelo_2.jpg";
 import { IoAddCircleOutline } from "react-icons/io5";
-function Card ({}) {
-
-
-    return (
-        <div className={style.card_main}>
-        <img className={style.image} src={imagen} alt="" />
-        <IoAddCircleOutline className={style.icon} />
-        
-        <span className={style.title}>Remera clasica exportman</span>
-        <span className={style.title}>$70</span>
-        </div>
-    )
+function Card({}) {
+  return (
+    <div className={style.card_main}>
+      <img className={style.image} src={imagen} alt="" />
+      <IoAddCircleOutline className={style.icon} />
+      <span className={style.title}>BLAZER TRAJE PATA DE GALLO</span>
+      <span className={style.price}>89,95 EUR</span>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
