bd2a85b0d255ee0f94841f627deb84689afd6b45
Juan Manuel <losredondos551@gmail.com>
2024-09-03 15:58:54 -0300

    detalle

--- src/pages/Detalle/Detalle.jsx
+++ src/pages/Detalle/Detalle.jsx
@@ -1,16 +1,31 @@
-import ExpandingSection from "../../components/ExpandingSection/ExpandingSection"
-import style from "./Detalle.module.css"
+import React, { useState, useEffect } from 'react';
+import ExpandingSection from "../../components/ExpandingSection/ExpandingSection";
+import style from "./Detalle.module.css";
 import imagenModelo1 from "../../assets/images/Ropa/Producto_4_Pantalon/Modelo/Modelo_1.jpg";
 import imagenModelo2 from "../../assets/images/Ropa/Producto_4_Pantalon/Modelo/Modelo_2.jpg";
 import imagenModelo3 from "../../assets/images/Ropa/Producto_4_Pantalon/Modelo/Modelo_3.jpg";
 import imagenModelo4 from "../../assets/images/Ropa/Producto_4_Pantalon/Modelo/Modelo_4.jpg";
 
-function Detalle(){
+function Detalle() {
+  const [isExpanded, setIsExpanded] = useState(false);
   const imagenes = [imagenModelo1, imagenModelo2, imagenModelo3, imagenModelo4];
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 30) {
+        setIsExpanded(true);
+      } else {
+        setIsExpanded(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <section className={style.detail_main}>
-      <div className={style.carousel}>
+      <div className={`${style.carousel} ${isExpanded ? style.expanded : ''}`}>
         {imagenes.map((item, index) => (
           <img
             key={index}
@@ -20,13 +35,12 @@ function Detalle(){
           />
         ))}
       </div>
-      <div className={style.detail}>
-
+      <div className={`${style.detail} ${isExpanded ? style.expanded : ''}`}>
+        {/* Aquí va el contenido de la tarjeta */}
       </div>
       <ExpandingSection/>
     </section>
-  )
+  );
 }
 
-
-export default Detalle
+export default Detalle;
