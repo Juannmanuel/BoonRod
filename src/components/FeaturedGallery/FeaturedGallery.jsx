5042ef3ef1a46a448d13fa940ace17bc2be11e7a
Juan Manuel <losredondos551@gmail.com>
2024-09-07 03:32:59 -0300

    card

--- src/components/FeaturedGallery/FeaturedGallery.jsx
+++ src/components/FeaturedGallery/FeaturedGallery.jsx
@@ -1,23 +1,7 @@
 import style from "./FeaturedGallery.module.css";
 
 function FeaturedGallery({ images, section, buttonText, description }) {
-  // const sections = [
 
-  //   {
-  //     section: "NEW COLLECTION",
-  //     buttonText: "Explorar",
-  //     images: [
-  //       imgC1,
-  //       imgC2,
-  //       imgC3,
-  //       imgC4,
-  //       imgC5,
-  //       imgC6,
-  //     ],
-  //     title: "Nuevas colecciones disponibles.",
-  //     description: "Explora nuestras últimas colecciones y mantente al día con las tendencias urbanas más recientes."
-  //   }
-  // ]
 
   return (
     <div className={style.home_container}>
