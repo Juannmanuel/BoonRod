bed5d6de4ff74c3cc4260e5e33d6ec657d063dfc
Juan Manuel <losredondos551@gmail.com>
2024-09-07 03:25:55 -0300

    card Home

--- src/components/FeaturedGallery/FeaturedGallery.jsx
+++ src/components/FeaturedGallery/FeaturedGallery.jsx
@@ -1,27 +1,49 @@
-import style from "./FeaturedGallery.module.css"
+import style from "./FeaturedGallery.module.css";
 
-function FeaturedGallery({images}){
+function FeaturedGallery({ images, section, buttonText, description }) {
+  // const sections = [
 
+  //   {
+  //     section: "NEW COLLECTION",
+  //     buttonText: "Explorar",
+  //     images: [
+  //       imgC1,
+  //       imgC2,
+  //       imgC3,
+  //       imgC4,
+  //       imgC5,
+  //       imgC6,
+  //     ],
+  //     title: "Nuevas colecciones disponibles.",
+  //     description: "Explora nuestras últimas colecciones y mantente al día con las tendencias urbanas más recientes."
+  //   }
+  // ]
 
-
-    return (
-        <div className={style.home_container}>
-        <div className={style.header}>
-          <img loading="lazy" src={images[0]} alt="" />
-        </div>
-        <div className={style.twoColumnsContainer}>
-          <img src={images[1]} alt="" />
-          <img src={images[2]} alt="" />
-        </div>
-        <div className={style.singleRowContainer}>
-          <img src={images[3]} alt="" />
-        </div>
-        <div className={style.twoColumnsContainer}>
-          <img src={images[4]} alt="" />
-          <img src={images[5]} alt="" />
+  return (
+    <div className={style.home_container}>
+      <div
+        className={style.header}
+        style={{ backgroundImage: `url(${images[0]})` }}
+      >
+        <div className={style.card}>
+          <h2 className={style.title}>{section}</h2>
+          <p className={style.description}>{description}</p>
+          <button className={style.btnAction}>{buttonText}</button>
         </div>
       </div>
-    )
+      <div className={style.twoColumnsContainer}>
+        <img src={images[1]} alt="" />
+        <img src={images[2]} alt="" />
+      </div>
+      <div className={style.singleRowContainer}>
+        <img src={images[3]} alt="" />
+      </div>
+      <div className={style.twoColumnsContainer}>
+        <img src={images[4]} alt="" />
+        <img src={images[5]} alt="" />
+      </div>
+    </div>
+  );
 }
 
-export default FeaturedGallery
+export default FeaturedGallery;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -28,9 +28,34 @@ import imgC6 from "../../assets/images/Categorias/newCollection/6.jpg";
 
 import FeaturedGallery from "../../components/FeaturedGallery/FeaturedGallery";
 function Home({ handleSlideChange, currentSlide }) {
+  const sections = [
+    {
+      section: "NEW COLLECTION",
+      buttonText: "Explorar",
+      images: [imgC1, imgC2, imgC3, imgC4, imgC5, imgC6],
+      title: "Nuevas colecciones disponibles.",
+      description:
+        "Explora nuestras últimas colecciones y mantente al día con las tendencias urbanas más recientes.",
+    },
+    {
+      section: "HOT SALE",
+      buttonText: "Hasta 30% OFF",
+      images: [imgD1, imgD2, imgD3, imgD4, imgD5],
+      title: "Nuestras ofertas exclusivas.",
+      description:
+        "Aprovecha las mejores ofertas y actualiza tu guardarropa sin gastar de más.",
+    },
+    {
+      section: "Lookbook",
+      buttonText: "Inspírate",
+      images: [imgF1, imgF2, imgF3, , imgF4, , imgF5, imgF6],
+      title: "Inspírate con nuestro Lookbook.",
+      description:
+        "Encuentra las mejores combinaciones y estilos para llevar la moda urbana a otro nivel.",
+    },
+  ];
 
-  
-  const sections = [[imgF1,imgF2,imgF3,imgF4,imgF5,imgF6], [imgD1,imgD2,imgD3,imgD4,imgD5,imgD6],  [imgC1,imgC2,imgC3,imgC4,imgC5,imgC6]]
+  // const sections = [[imgF1,imgF2,imgF3,imgF4,imgF5,imgF6], [imgD1,imgD2,imgD3,imgD4,imgD5,imgD6],  [imgC1,imgC2,imgC3,imgC4,imgC5,imgC6]]
   const [isLouding, setIsLouding] = useState(true);
   setTimeout(() => {
     setIsLouding(false);
@@ -42,7 +67,15 @@ function Home({ handleSlideChange, currentSlide }) {
         handleSlideChange={handleSlideChange}
         currentSlide={currentSlide}
       />
-      {sections.map((item, index) => <FeaturedGallery images={item} key={index}/>)}
+      {sections.map((item, index) => (
+        <FeaturedGallery
+          images={item.images}
+          section={item.section}
+          buttonText={item.buttonText}
+          description={item.description}
+          key={index}
+        />
+      ))}
 
       {/* {isLouding ? (
         <Louder />
