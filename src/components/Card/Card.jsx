26e50a4cc107779a286f883784e6c363d66907b7
Juan Manuel <losredondos551@gmail.com>
2024-09-09 16:43:37 -0300

    catalogo

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,11 +2,11 @@ import style from "./Card.module.css";
 import imagen from "../../assets/images/Ropa/Producto_5_Remera/Modelo/Modelo_2.jpg";
 import { IoAddCircleOutline } from "react-icons/io5";
 function Card({images, title, price, section}) {
-  console.log(images.models[0],"card");
+
   
   return (
     <div className={style.card_main}>
-      <img className={style.image} src={images.models[0]} alt="" />
+      <img className={style.image} src={images} alt="" />
       <IoAddCircleOutline className={style.icon} />
       <span className={style.title}>{title}</span>
       <span className={style.price}>{price}EUR</span>
--- src/components/FeatureBlock/FeatureBlock.jsx
+++ src/components/FeatureBlock/FeatureBlock.jsx
@@ -1,18 +1,32 @@
-import RecommendedProducts from "../RecommendedProducts/RecommendedProducts"
-import style from "./FeatureBlock.module.css"
+import Card from "../Card/Card";
+import RecommendedProducts from "../RecommendedProducts/RecommendedProducts";
+import style from "./FeatureBlock.module.css";
+import image from "../../assets/images/imagesProductsAndModels/product-2/models/model-1.jpg";
 
-
-function FeatureBlock ({section}) {
-
-
-
-    return (
-        <section className={style.featureBlock_main} data-section={section}>
-        {/* <RecommendedProducts/> */}
-        </section>
-    )
+function FeatureBlock({ section }) {
+  const itemsRopa = [
+    { images: image, title: "CAMISETA BOXY FIT", price: "1500" },
+    { images:  image, title: "CAMISETA BOXY FIT", price: "1500" },
+    { images:  image, title: "CAMISETA BOXY FIT", price: "1500" },
+    { images:  image, title: "CAMISETA BOXY FIT", price: "1500" },
+    { images:  image, title: "CAMISETA BOXY FIT", price: "1500" },
+  ];
+  return (
+    <section className={style.featureBlock_main} data-section={section}>
+      <div className={style.container}>
+        <div className={style.heading}>Colección Urbana</div>
+        <div className={style.description}>
+          Vístete con las mejores tendencias de la moda urbana, inspiradas en el
+          movimiento y la libertad de la vida moderna.
+        </div>
+        <div className={style.carousel}>
+          {itemsRopa.map((item, index) => (
+            <Card images={item.images} title={item.title}price={item.price} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
-
-
-export default FeatureBlock
+export default FeatureBlock;
