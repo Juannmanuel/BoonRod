98bee0e5e1e7912143190ef19e46e05c5721ed88
Juan Manuel <losredondos551@gmail.com>
2024-09-10 17:27:10 -0300

    Listorti

--- src/components/DesktopView/DesktopView.jsx
+++ src/components/DesktopView/DesktopView.jsx
@@ -1,14 +1,22 @@
-import style from "./DesktopView.module.css"
+import style from "./DesktopView.module.css";
+import qr from "../../assets/images/QR/qr.png"
+function DesktopView() {
+  return (
+    <div className={style.desktopViewContainer}>
+      <h1 className={style.desktopViewTitle}>Esta página está en mantenimiento</h1>
+      <p className={style.desktopViewText}>
+        Actualmente solo está disponible en la vista móvil.
+      </p>
+      <p className={style.desktopViewText}>
+        Escanea el siguiente código QR con tu celular para acceder al sitio:
+      </p>
+      <img
+        className={style.qrImage}
+        src={qr}
+        alt="Código QR para acceder al sitio"
+      />
+    </div>
+  );
+}
 
-function DesktopView()  {
-    return (
-      <div style={{ textAlign: 'center', padding: '20px' }}>
-        <h1>Esta página está en mantenimiento</h1>
-        <p>Actualmente solo está disponible en la vista móvil.</p>
-        <p>Escanea el siguiente código QR con tu celular para acceder al sitio:</p>
-        <img src="ruta-del-codigo-QR.png" alt="Código QR para acceder al sitio" />
-      </div>
-    );
-  };
-
-  export default DesktopView
+export default DesktopView;
