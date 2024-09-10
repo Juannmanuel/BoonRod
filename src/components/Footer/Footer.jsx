d982813922bcd08c80d879585a86c6bacaaaeb9e
Juan Manuel <losredondos551@gmail.com>
2024-09-10 15:47:27 -0300

    Footer ok

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
+import SocialLinks from "../SocialLinks/SocialLinks";
 import style from "./Footer.module.css";
+import { PiTrademarkRegisteredLight } from "react-icons/pi";
 
 function Footer() {
   return (
     <footer className={style.footer_main}>
-      <h2>Footer</h2>
+      <h2 className={style.title}>
+        BOONROD <PiTrademarkRegisteredLight />
+      </h2>
+      <SocialLinks />
+      <div className={style.contact}>
+        <Link style={{color: "black", textDecoration: "none"}} to={"https://juan-manuel.vercel.app"}>Quiero mi página</Link>
+      </div>
     </footer>
   );
 }
