d9fe5040ef8041f176154b322366b0150199e9a3
Juan Manuel <losredondos551@gmail.com>
2024-09-25 22:00:57 -0300

    About

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import style from "./About.module.css";
-
+import imageLocal from "../../assets/images/logo/local_image.jpeg";
 import SocialLinks from "../../components/SocialLinks/SocialLinks";
 import CustomerSupport from "../../components/CustomerSupport/CustomerSupport";
 
@@ -9,33 +9,33 @@ const About = () => {
   const longitude = "-68.85343209005165";
   return (
     <div className={style.aboutContainer}>
-      <div className={style.wave_container}></div>
+      <div className={style.wave_container}>
+        <img src={imageLocal} alt="" />
+      </div>
+      <div className={style.container_content}>
+        <h1 className={style.title}>¿Quiénes somos?</h1>
+        <p className={style.description}>
+          Somos una empresa dedicada a la venta de indumentaria masculina,
+          enfocada en ofrecer productos de alta calidad que reflejan un estilo
+          urbano y moderno. Somos una empresa dedicada a la venta de
+          indumentaria masculina, enfocada en ofrecer productos de alta calidad
+          que reflejan un estilo urbano y moderno. Somos una empresa dedicada a
+          la venta de indumentaria masculina, enfocada en ofrecer productos de
+          alta calidad que reflejan un estilo urbano y moderno.
+        </p>
 
-      <h1 className={style.title}>¿Quiénes somos?</h1>
-      <p className={style.description}>
-        Somos una empresa dedicada a la venta de indumentaria masculina,
-        enfocada en ofrecer productos de alta calidad que reflejan un estilo
-        urbano y moderno.
-        Somos una empresa dedicada a la venta de indumentaria masculina,
-        enfocada en ofrecer productos de alta calidad que reflejan un estilo
-        urbano y moderno.
-        Somos una empresa dedicada a la venta de indumentaria masculina,
-        enfocada en ofrecer productos de alta calidad que reflejan un estilo
-        urbano y moderno.
-      </p>
-
-
-      <button
-        className={style.mapButton}
-        onClick={() =>
-          window.open(
-            `https://www.google.com/maps?q=${latitude},${longitude}`,
-            "_blank"
-          )
-        }
-      >
-        Ver ubicación en Google Maps
-      </button>
+        <button
+          className={style.mapButton}
+          onClick={() =>
+            window.open(
+              `https://www.google.com/maps?q=${latitude},${longitude}`,
+              "_blank"
+            )
+          }
+        >
+          Ver ubicación en Google Maps
+        </button>
+      </div>
       <SocialLinks />
     </div>
   );
