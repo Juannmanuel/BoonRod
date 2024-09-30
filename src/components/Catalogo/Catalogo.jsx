b2980aede396b8319a890681030b460d68db7af3
Juan Manuel <losredondos551@gmail.com>
2024-09-30 19:36:17 -0300

    catalogo

--- src/components/Catalogo/Catalogo.jsx
+++ src/components/Catalogo/Catalogo.jsx
@@ -13,7 +13,14 @@ function Catalogo() {
   const [showDiscounted, setShowDiscounted] = useState(false);
 
   // Array de tipos de producto
-  let typeOfProduct = ["Todo", "Pantalon", "Campera", "Buzo", "Remera", "Zapatillas"];
+  let typeOfProduct = [
+    "Todo",
+    "Pantalon",
+    "Campera",
+    "Buzo",
+    "Remera",
+    "Zapatillas",
+  ];
 
   // Función para manejar el cambio de tipo
   const handleTypeChange = (type) => {
@@ -27,35 +34,46 @@ function Catalogo() {
 
   // Filtrar productos por tipo seleccionado y ofertas
   const filteredProducts = products
-    .filter(product => selectedType === "Todo" || product.type === selectedType)
-    .filter(product => !showDiscounted || product.discount.isDiscounted);
+    .filter(
+      (product) => selectedType === "Todo" || product.type === selectedType
+    )
+    .filter((product) => !showDiscounted || product.discount.isDiscounted);
 
   return (
     <section className={style.catalogo_main}>
       <nav className={style.nav_main}>
         <LaNavbar />
+        <section className={style.container_filter_products}>
+          <div className={style.container_types}>
+            {typeOfProduct.map((item, index) => {
+              return (
+                <span
+                  className={`${style.type} ${
+                    selectedType === item ? style.type_selected : ""
+                  }`}
+                  key={index}
+                  onClick={() => handleTypeChange(item)}
+                >
+                  {item}
+                </span>
+              );
+            })}
+          </div>
+          <div className={style.container_button}>
+            <span
+              className={`${style.type_discount} ${
+                showDiscounted ? style.type_discount_selected : ""
+              }`}
+              onClick={handleShowDiscounted}
+            >
+              {showDiscounted ? "Todo" : "Ofertas"}
+            </span>
+          </div>
+        </section>
       </nav>
-      <section className={style.container_filter_products}>
-        <div className={style.container_types}>
-          {typeOfProduct.map((item, index) => {
-            return (
-              <span
-                className={`${style.type} ${selectedType === item ? style.type_selected : ""}`}
-                key={index}
-                onClick={() => handleTypeChange(item)}
-              >
-                {item}
-              </span>
-            );
-          })}
-        </div>
-        <div className={style.container_button}>
-          <span className={`${style.type_discount} ${showDiscounted ? style.type_discount_selected : ""}`} onClick={handleShowDiscounted}>
-            {showDiscounted ? "Todo" : "Ofertas"}
-          </span>
-        </div>
+      <section className={style.container_products}>
+        <RecommendedProducts products={filteredProducts} section={"catalogo"} />
       </section>
-      <RecommendedProducts products={filteredProducts} section={"catalogo"} />
       <Footer />
     </section>
   );
