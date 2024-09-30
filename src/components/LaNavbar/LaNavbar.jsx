c4c6305393696018458e07206d4daa6497e49a5c
Juan Manuel <losredondos551@gmail.com>
2024-09-29 21:48:15 -0300

    burgerMenu

--- src/components/LaNavbar/LaNavbar.jsx
+++ src/components/LaNavbar/LaNavbar.jsx
@@ -3,15 +3,17 @@ import style from "./LaNavbar.module.css";
 import boonrodText from "../../assets/images/logo/logo_tipografia_boonrod_claro.png";
 import { IoMenu } from "react-icons/io5";
 import { CiShoppingCart } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
 import { useSelector } from "react-redux";
 import { useState } from "react";
+import SocialLinks from "../SocialLinks/SocialLinks";
 const LaNavbar = ({ currentSection, ref }) => {
+  const sections = useSelector((state) => state.backUpSections);
   const isScrollingUp = useSelector((state) => state.isScrollingUp);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  function toggleMenu(){
+  function toggleMenu() {
     console.log("entré");
-    setIsMenuOpen(!isMenuOpen)
-    
+    setIsMenuOpen(!isMenuOpen);
   }
   return (
     <nav
@@ -23,11 +25,11 @@ const LaNavbar = ({ currentSection, ref }) => {
       }
     >
       <div className={style.nav_actions_container}>
-        <IoMenu
-          style={{ color: isScrollingUp ? "black" : "white" }}
-          className={style.icon}
-          onClick={()=> toggleMenu()}
-        />
+        {isMenuOpen ? (
+          <IoMdClose className={style.icon} onClick={() => toggleMenu()} />
+        ) : (
+          <IoMenu className={style.icon} onClick={() => toggleMenu()} />
+        )}
 
         <div className={style.categorie}>
           <img src={boonrodText} />
@@ -35,12 +37,26 @@ const LaNavbar = ({ currentSection, ref }) => {
         <CiShoppingCart className={style.icon} />
       </div>
       {/* menú amburgesa */}
-      { isMenuOpen && <div className={`${style.hamburger_menu_container} ${isMenuOpen ? style.open : style.closed}`}>
-        <div className={style.container_links}>
-
+      {isMenuOpen && (
+        <div
+          className={`${style.hamburger_menu_container} ${
+            isMenuOpen ? style.open : style.close
+          }`}
+        >
+          <div className={style.container_links}>
+            {sections?.map((item, index) => {
+              return (
+                <div key={index} className={style.link_item}>
+                  <span>{item.sectionName}</span>
+                </div>
+              );
+            })}
+            <div className={style.link_item}>
+              <SocialLinks />
+            </div>
+          </div>
         </div>
-      </div>
-    }
+      )}
     </nav>
   );
 };
